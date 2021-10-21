e23847392edf7a7079fe7c25c7c0fbb7cbc9fb8c
kyuung <mel1521@naver.com>
2021-10-22 02:02:10 +0900

    correct signup/in logic

--- src/components/SignModal.js
+++ src/components/SignModal.js
@@ -5,208 +5,205 @@ import Text from '../elements/Text';
 const SignModal = (props) => {
 	const phoneNum = /^[0-9]+$/;
 	const pwdNum = /^[0-9]+$/;
+	const nameChk = /^[가-힣a-zA-Z]+$/;
+	const emailChk = /^[a-z0-9\.\-_]+@([a-z0-9\-]+\.)+[a-z]{2,6}$/;
 
-	// ! 모달 view 관련 state
-	const { _signUpStatus, _signInStatus } = props;
-	const [signUpStatus, setSignUpStatus] = React.useState(_signUpStatus);
-	const [signInStatus, setSignInStatus] = React.useState(_signInStatus);
+	const { _signStatus } = props;
 
-	// ! 회원가입 state
-	const [signUpTel, setSignUpTel] = React.useState('');
-	const [signUpPwd, setSignUpPwd] = React.useState('');
-	const [signUpPwd_chk, setSignUpPwd_chk] = React.useState('');
+	const [signStatus, setSignStatus] = React.useState(_signStatus);
+	const [basicTel, setBasicTel] = React.useState('');
 
-	const submitSignUp = () => {
-		if (signUpPwd !== signUpPwd_chk) {
-			window.alert('이전 패스워드와 비밀번호가 맞지 않습니다.');
+	const [signInModal, setSignInModal] = React.useState(false);
+	const [signUpModal, setSignUpModal] = React.useState(false);
+
+	// * 로그인 input
+	const [signInPin, setSignInPin] = React.useState('');
+	const [signInPhone, setSignInPhone] = React.useState('');
+
+	// * 회원가입 Input
+	const [signUpName, setSignUpName] = React.useState('');
+	const [signUpMail, setSignUpMail] = React.useState('');
+	const [signUpPin, setSignUpPin] = React.useState('');
+	const [signUpPin_chk, setSignUpPin_chk] = React.useState('');
+
+	const phoneConfirm = () => {
+		if (phoneNum.test(basicTel) === false) {
+			window.alert('핸드폰 번호를 입력해주세요.');
 			return;
-		} else if (pwdNum.test(signUpPwd_chk) === false) {
-			window.alert('비밀번호는 숫자로 입력해주세요.');
-			return;
-		} else if (signUpPwd_chk.length !== 6) {
-			window.alert('비밀번호는 숫자로 6자 입력해주세요.');
+		} else if (basicTel.length !== 11) {
+			window.alert('핸드폰 번호 11자를 입력해주세요.');
 			return;
 		}
-		setSignUpStatus(signUpStatus + 1);
-		// TODO 이쪽에서 회원가입 신청하기 ! ~!~!~!
+		const test = window.confirm('true/false');
+		if (test) {
+			// true : 로그인
+			setSignInModal(true);
+			setSignStatus(false);
+		} else if (!test) {
+			// false : 회원가입
+			setSignUpModal(true);
+			setSignStatus(false);
+		}
 	};
 
-	// ! 로그인 state
-	const [signInTel, setSignInTel] = React.useState('');
-	const [signInPwd, setSignInPwd] = React.useState('');
-
-	const submitSignIn = () => {
-		if (pwdNum.test(signInPwd) === false) {
-			window.alert('비밀번호는 숫자로 입력해주세요.');
+	const logIn = () => {
+		if (phoneNum.test(signInPhone) === false || signInPhone.length !== 11) {
+			window.alert('핸드폰 번호로 11자 입력해주세요.');
 			return;
-		} else if (signInPwd.length !== 6) {
-			window.alert('비밀번호는 6자로 입력해주세요.');
+		} else if (pwdNum.test(signInPin) === false || signInPin.length !== 6) {
+			window.alert('핀번호는 숫자로 6자 입력해주세요.');
 			return;
 		}
-		// TODO 이쪽에서 로그인 신청하기 ! ~!~!~!
-		setSignInStatus(signInStatus + 1);
-		console.log(signInTel, signInPwd);
+		console.log(signInPin, signInPhone);
+		setSignInModal(false);
+	};
+
+	const signUp = () => {
+		if (!nameChk.test(signUpName)) {
+			window.alert('이름은 한글이나 영문으로 입력해주세요.');
+			return;
+		} else if (!emailChk.test(signUpMail)) {
+			window.alert('이메일은 이메일 형식으로 입력해주세요.');
+			return;
+		} else if (!pwdNum.test(signUpPin)) {
+			window.alert('핀번호는 숫자로 입력해주세요.');
+			return;
+		} else if (signUpPin !== signUpPin_chk) {
+			window.alert('핀번호와 핀번호 확인이 맞지 않습니다.');
+			return;
+		}
 	};
 
 	return (
 		<>
-			{signUpStatus > 0 && (
+			{signStatus && (
 				<SignUp>
-					<XBtn onClick={() => setSignUpStatus(0)}>X</XBtn>
+					<XBtn
+						onClick={() => {
+							setSignStatus(false);
+						}}
+					>
+						X
+					</XBtn>
 					<Text
 						bold="700"
-						fontSize="2rem"
-						others="margin-bottom:2rem"
+						fontSize="1.8rem"
+						others="margin-bottom:3rem;"
 					>
-						회원가입
+						로그인 또는 회원가입
 					</Text>
-					<Text bold="700" fontSize="1.4rem">
-						{signUpStatus === 1 && '전화번호를 입력하세요'}
-						{signUpStatus === 2 && '패스워드를 6자 입력하세요'}
-						{signUpStatus === 3 && '패스워드를 6자 확인해주세요'}
-						{signUpStatus === 4 && (
-							<i className="fab fa-airbnb">
-								<span> </span>회원가입이 완료되었습니다.
-							</i>
-						)}
+					<Text bold="700" fontSize="1.2rem">
+						전화번호를 입력하세요.
 					</Text>
-					{signUpStatus === 1 && (
-						<Input
-							maxLength="11"
-							onChange={(e) => setSignUpTel(e.target.value)}
-						/>
-					)}
-					{signUpStatus === 2 && (
-						<Input
-							maxLength="6"
-							onChange={(e) => setSignUpPwd(e.target.value)}
-							type="password"
-						/>
-					)}
-					{signUpStatus === 3 && (
-						<Input
-							maxLength="6"
-							onChange={(e) => setSignUpPwd_chk(e.target.value)}
-							type="password"
-						/>
-					)}
-					{signUpStatus === 1 && (
-						<ContinueBtn
-							onClick={() => {
-								if (phoneNum.test(signUpTel) === false) {
-									window.alert('핸드폰 번호를 입력해주세요.');
-									return;
-								} else if (signUpTel.length !== 11) {
-									window.alert(
-										'핸드폰 번호 11자를 입력해주세요.'
-									);
-									return;
-								}
-								// TODO 첫번째 회원가입 요청해서 유저인지 아닌지 판별
-								setSignUpStatus(signUpStatus + 1);
-							}}
-						>
-							계속
-						</ContinueBtn>
-					)}
-					{signUpStatus === 2 && (
-						<ContinueBtn
-							onClick={() => {
-								if (pwdNum.test(signUpPwd) === false) {
-									window.alert('숫자만 입력해주세요.');
-									return;
-								} else if (signUpPwd.length !== 6) {
-									window.alert('6자 입력해주세요');
-									return;
-								}
-								setSignUpStatus(signUpStatus + 1);
-							}}
-						>
-							계속
-						</ContinueBtn>
-					)}
-					{signUpStatus === 3 && (
-						<ContinueBtn onClick={submitSignUp}>
-							회원가입 하기
-						</ContinueBtn>
-					)}
-					{signUpStatus === 4 && (
-						<ContinueBtn
-							onClick={() => {
-								setSignUpStatus(0);
-							}}
-						>
-							사이트 둘러보기
-						</ContinueBtn>
-					)}
+					<Input
+						maxLength="11"
+						onChange={(e) => setBasicTel(e.target.value)}
+					/>
+					<ContinueBtn onClick={phoneConfirm}>제출하기</ContinueBtn>
 				</SignUp>
 			)}
-			{signInStatus > 0 && (
+
+			{signInModal && (
 				<SignUp>
-					<XBtn onClick={() => setSignInStatus(0)}>X</XBtn>
+					<XBtn
+						onClick={() => {
+							setSignInModal(false);
+						}}
+					>
+						X
+					</XBtn>
 					<Text
 						bold="700"
-						fontSize="2rem"
-						others="margin-bottom:2rem"
+						fontSize="1.8rem"
+						others="margin-bottom:3rem;"
 					>
 						로그인
 					</Text>
-					<Text bold="700" fontSize="1.4rem">
-						{signInStatus === 1 && '전화번호를 입력하세요'}
-						{signInStatus === 2 && '패스워드를 6자 입력하세요'}
-						{signInStatus === 3 && (
-							<i className="fab fa-airbnb">
-								<span> </span>로그인이 완료되었습니다.
-							</i>
-						)}
+					<Text bold="700" fontSize="1.2rem">
+						핸드폰 번호를 입력하세요.
 					</Text>
-					{signInStatus === 1 && (
-						<Input
-							maxLength="11"
-							onChange={(e) => setSignInTel(e.target.value)}
-						/>
-					)}
-					{signInStatus === 2 && (
-						<Input
-							maxLength="6"
-							type="password"
-							onChange={(e) => setSignInPwd(e.target.value)}
-						/>
-					)}
+					<Input
+						maxLength="11"
+						onChange={(e) => setSignInPhone(e.target.value)}
+					/>
+					<Text
+						bold="700"
+						fontSize="1.2rem"
+						others="margin-top:2rem;"
+					>
+						핀 번호를 입력하세요.
+					</Text>
+					<Input
+						maxLength="6"
+						onChange={(e) => setSignInPin(e.target.value)}
+						type="password"
+					/>
+					<ContinueBtn style={{ marginTop: '2rem' }} onClick={logIn}>
+						로그인
+					</ContinueBtn>
+				</SignUp>
+			)}
 
-					{signInStatus === 1 && (
-						<ContinueBtn
-							onClick={() => {
-								if (phoneNum.test(signInTel) === false) {
-									window.alert('핸드폰 번호를 입력해주세요.');
-									return;
-								} else if (signInTel.length !== 11) {
-									window.alert(
-										'핸드폰 번호 11자를 입력해주세요.'
-									);
-									return;
-								}
-								// TODO 첫번째 회원가입 요청해서 유저인지 아닌지 판별
-								setSignInStatus(signInStatus + 1);
-							}}
-						>
-							계속
-						</ContinueBtn>
-					)}
-					{signInStatus === 2 && (
-						<ContinueBtn onClick={submitSignIn}>
-							로그인 하기
-						</ContinueBtn>
-					)}
-					{signInStatus === 3 && (
-						<ContinueBtn
-							onClick={() => {
-								setSignInStatus(0);
-							}}
-						>
-							사이트 둘러보기
-						</ContinueBtn>
-					)}
+			{signUpModal && (
+				<SignUp>
+					<XBtn
+						onClick={() => {
+							setSignUpModal(false);
+						}}
+					>
+						X
+					</XBtn>
+					<Text
+						bold="700"
+						fontSize="1.8rem"
+						others="margin-bottom:1rem;"
+					>
+						회원가입
+					</Text>
+					<Text
+						bold="700"
+						fontSize="1.2rem"
+						others="margin-top:1rem;"
+					>
+						이름
+					</Text>
+					<Input onChange={(e) => setSignUpName(e.target.value)} />
+					<Text
+						bold="700"
+						fontSize="1.2rem"
+						others="margin-top:1rem;"
+					>
+						E-mail
+					</Text>
+					<Input onChange={(e) => setSignUpMail(e.target.value)} />
+					<Text
+						maxLength="6"
+						bold="700"
+						fontSize="1.2rem"
+						others="margin-top:1rem;"
+					>
+						핀 번호를 입력하세요.
+					</Text>
+					<Input
+						maxLength="6"
+						type="password"
+						onChange={(e) => setSignUpPin(e.target.value)}
+					/>
+					<Text
+						bold="700"
+						fontSize="1.2rem"
+						others="margin-top:1rem;"
+					>
+						핀 번호를 한번 더 입력하세요.
+					</Text>
+					<Input
+						maxLength="6"
+						type="password"
+						onChange={(e) => setSignUpPin_chk(e.target.value)}
+					/>
+					<ContinueBtn style={{ marginTop: '2rem' }} onClick={signUp}>
+						회원가입
+					</ContinueBtn>
 				</SignUp>
 			)}
 		</>
@@ -215,7 +212,7 @@ const SignModal = (props) => {
 
 const SignUp = styled.div`
 	width: 30rem;
-	height: 35rem;
+	height: 40rem;
 	background: #fff;
 	display: flex;
 	justify-content: center;
