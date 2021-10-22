033cff4aa04124db7c869c8f69e0bcbcf3369505
kyuung <mel1521@naver.com>
2021-10-22 21:39:56 +0900

    Header Check Login/Logout

--- src/Shared/utils.js
+++ src/Shared/utils.js
@@ -1,17 +1,17 @@
 /* 개발모드에서 logger */
-export const logger = msg => {
-  if (process.env.NODE_ENV === 'production') {
-    return
-  }
-  console.log(msg)
-}
+export const logger = (msg) => {
+	if (process.env.NODE_ENV === 'production') {
+		return;
+	}
+	console.log(msg);
+};
 
 /* localStorage에서 토큰 가져오기 */
-export const getToken = async () => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    return `Bearer ${token}`
-  } else {
-    return null
-  }
-}
+export const getToken = () => {
+	const token = localStorage.getItem('token');
+	if (token) {
+		return `Bearer ${token}`;
+	} else {
+		return null;
+	}
+};
